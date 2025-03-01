9ab7352b3382d3136f5a7985356ffdb1ffbe77fa
Adithya R <gh0strider.2k18.reborn@gmail.com>
2025-03-01 13:33:45 -0300

    config: Build LMO Freeform app and sidebar
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: GXC 2356 <gxcdev2356@gmail.com>

--- config/common.mk
+++ config/common.mk
@@ -91,6 +91,11 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.biometrics.face.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/android.hardware.biometrics.face.xml
 endif
 
+# LMOFreeForm
+PRODUCT_PACKAGES += \
+    LMOFreeform \
+    LMOFreeformSidebar
+
 # Enforce privapp-permissions whitelist
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     ro.control_privapp_permissions=enforce
